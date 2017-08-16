58eee2730a4e85f338443a491b7e0bb48410bb00
Skylar Moore <nickgray0@gmail.com>
2017-08-16 06:30:05 -0700

    IO

---
+++ IO.py
@@ -0,0 +1,11 @@
+import numpy
+
+def LoadRawBinaryGrayscaleSequence(Address, Width, Height, Length, PixelType='float32'):
+    Sequence = numpy.memmap(Address, dtype=PixelType, mode='r', shape=(Width, Height, Length))
+    Sequence = numpy.array(Sequence, dtype='float64')
+    return Sequence
+
+def ExportRawBinaryGrayscaleSequence(Data, Address, PixelType='float32'):
+    Sequence = numpy.memmap(Address, dtype=PixelType, mode='w+', shape=Data.shape)
+    Sequence[:] = numpy.array(Data, dtype=PixelType)[:]
+    del Sequence
